95c7702615c9500e48728b5db25551dfab43425b
sarabluekens <sara.bluekens@student.howest.be>
2021-04-30 01:36:12 +0200

    contentful + netlify test: first fetch hardcoded data

---
+++ components/Message.js
@@ -0,0 +1,15 @@
+
+function Message({ mood, message, title }) {
+
+
+  return (
+    <div className="message">
+        <h1 className={title}>{title}</h1>
+        <p>Message:{message}</p>
+        <p>I feel:{mood}</p>
+   
+    </div>
+  )
+}
+
+export default Message
