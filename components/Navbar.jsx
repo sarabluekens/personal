0a782a7efccd481b265852c470476a08b7f0cec4
sarabluekens <sara.bluekens@student.howest.be>
2021-05-01 03:44:27 +0200

    components + css start added

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -9,7 +9,6 @@ const Navbar = () => {
                 <Link href="/"><li className={styles.nav__list__item} >Create message</li></Link>
                 <Link href="/about"><li className={styles.nav__list__item} >About</li></Link>
                 <Link href="/somethingelse"><li className={styles.nav__list__item} >Something else</li></Link>
-                <Link href="/test"><a className={styles.nav__list__item} > test this</a></Link>
             </ul>
         </nav>
      );
