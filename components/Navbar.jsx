497710a70791b801b088bb7fe938ec6406ff2d9d
sarabluekens <sara.bluekens@student.howest.be>
2021-05-02 18:01:19 +0200

    images bug, try fix

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -7,7 +7,7 @@ export default function Navbar() {
     return ( 
         <nav className={styles.nav}>
             <ul className={styles.nav__list}>
-                <Image className={styles.nav__list__img} src="/img/logo.svg" width="100" height="100"/>
+                <Image className={styles.nav__list__img} src="/img/logo.png" width="100" height="100"/>
                 <Link href="/"><li className={styles.nav__list__item} >Bericht maken</li></Link>
                 <Link href="/about"><li className={styles.nav__list__item} >0ver</li></Link>
             </ul>
